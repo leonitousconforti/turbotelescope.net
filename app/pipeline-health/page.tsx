456913c71db2565d25d8e1f4478a391fab9e9cc2
Leo Conforti <leo@leoconforti.us>
2024-11-14 18:09:12 +0000

    do not export dynamic

--- app/pipeline-health/page.tsx
+++ app/pipeline-health/page.tsx
@@ -1,8 +1,7 @@
 import { PipelineHealth } from "@/components/PipelineHealth";
 import { ThemeToggle } from "@/components/ThemeToggle";
-import dynamic from "next/dynamic";
 
-const page = () => {
+export default function page() {
     return (
         <>
             <div className="fixed bottom-5 right-5 z-50">
@@ -11,7 +10,4 @@ const page = () => {
             <PipelineHealth />
         </>
     );
-};
-
-// FIXME: bad bad bad bad bad
-export default dynamic(() => Promise.resolve(page), { ssr: false });
+}
